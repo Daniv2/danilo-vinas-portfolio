939825367d39ab9b65e63a08bf41ff558364c0ef
Daniv2 <125231943+Daniv2@users.noreply.github.com>
2024-08-01 23:19:30 -0600

    codes

---
+++ danilo-vinas-portfolio/src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section>
+      <h1>Welcome to My Portfolio</h1>
+      <p>I am a Web Designer and Developer</p>
+    </section>
+  );
+};
+
+export default Hero;
