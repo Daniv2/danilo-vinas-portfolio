1af5d5c34f1ec9e2d7ecfe38a7abf2eba5872942
Daniv2 <125231943+Daniv2@users.noreply.github.com>
2024-08-01 23:34:37 -0600

    Update App.js

--- danilo-vinas-portfolio/src/App.js
+++ danilo-vinas-portfolio/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero.js';
+import Hero from './components/Hero';
 import Projects from './components/Projects';
 import AboutMe from './components/AboutMe';
 import Contact from './components/Contact';
@@ -9,6 +9,7 @@ import Design from './components/Design';
 import Develop from './components/Develop';
 import Blog from './components/Blog';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
